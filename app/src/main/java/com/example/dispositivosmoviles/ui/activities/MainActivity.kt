176adb4915b79598450e6e23e69749a17fafee54
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-07-22 08:57:12 -0500

    Coarse y fine location

--- app/src/main/java/com/example/dispositivosmoviles/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/dispositivosmoviles/ui/activities/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.dispositivosmoviles.ui.activities
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
+import android.location.Geocoder
 import android.net.Uri
 import android.os.Bundle
 import android.speech.RecognizerIntent
@@ -10,6 +13,7 @@ import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.PermissionChecker.PermissionResult
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.stringPreferencesKey
@@ -20,6 +24,8 @@ import androidx.lifecycle.lifecycleScope
 import com.example.dispositivosmoviles.R
 import com.example.dispositivosmoviles.databinding.ActivityMainBinding
 import com.example.dispositivosmoviles.ui.validator.LoginValidator
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -32,12 +38,16 @@ val Context.dataStore: DataStore<Preferences>
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    //interfaz que nos va a permitir acceder a la ubicacion del ususario
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onStart() {
@@ -49,6 +59,7 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 
+    @SuppressLint("MissingPermission")
     private fun initClass() {
         binding.btnLogin.setOnClickListener {
 
@@ -77,9 +88,56 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        val locationContract = registerForActivityResult(RequestPermission()) {
+            isGranted ->
+            when(isGranted){
+                true -> {
+                    fusedLocationProviderClient.lastLocation.addOnSuccessListener {
+                        it.longitude
+                        it.latitude
+
+                        val a = Geocoder(this)
+                        a.getFromLocation(it.latitude, it.longitude, 1)
+                    }
+
+                    /*val task = fusedLocationProviderClient.lastLocation
+                    task.addOnSuccessListener {
+                        if(task.result != null) {
+                            Snackbar.make(binding.txtName,
+                                "${it.latitude}, ${it.longitude}",
+                                Snackbar.LENGTH_LONG)
+                                .show()
+                        } else {
+                            Snackbar.make(binding.txtName,
+                                "Encienda el GPS, por favor",
+                                Snackbar.LENGTH_LONG)
+                                .show()
+                        }
+                    }*/
+                }
+
+                //Informa al usuario de porque se necesita los permisos
+                shouldShowRequestPermissionRationale(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) -> {
+                    Snackbar.make(binding.txtName,
+                        "Ayude con el permiso",
+                        Snackbar.LENGTH_LONG)
+                        .show()
+                }
+                false -> {
+                    Snackbar.make(binding.txtName, "Permiso denegado", Snackbar.LENGTH_LONG)
+                    .show()
+                }
+            }
+
+        }
+
         binding.btnTwitter.setOnClickListener {
 
-            val intent = Intent(
+            locationContract.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+
+            /*val intent = Intent(
                 Intent.ACTION_WEB_SEARCH
             )
             //abre la barra de busqueda de Google
@@ -89,7 +147,7 @@ class MainActivity : AppCompatActivity() {
             )
             //busca steam en el navegador
             intent.putExtra(SearchManager.QUERY, "steam")
-            startActivity(intent)
+            startActivity(intent)*/
         }
 
         val appResultLocal = registerForActivityResult(StartActivityForResult()) {
@@ -171,7 +229,7 @@ class MainActivity : AppCompatActivity() {
                 RecognizerIntent.LANGUAGE_MODEL_FREE_FORM //modelo de lenguaje libre
             )
             intentSpeech.putExtra(
-                RecognizerIntent.EXTRA_LANGUAGE, //en que idioma va a hblar
+                RecognizerIntent.EXTRA_LANGUAGE, //en que idioma va a hablar
                 Locale.getDefault() //toma el lenguaje del dispositivo
             )
             intentSpeech.putExtra(
